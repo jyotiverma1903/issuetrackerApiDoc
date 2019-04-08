51dc719424eb90d98ea7a3af9b8c9cdb17ff3ff6
jyotiverma1903 <jyotiverma.eca@gmail.com>
2019-04-08 21:04:58 +0530

    first commit

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Issue Tracker by Jyoti Verma",
+  "version": "0.0.1",
+  "description": "Documentation for the Issue Tracker",
+  "title": "Issue Tracker API's",
+  "url": "http://apidoc.jyotivermprojects.info",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-04-08T15:30:22.540Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
